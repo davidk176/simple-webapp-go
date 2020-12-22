a7d7eb750d8d2d59f77a184d84a42b4492fefa17
David Karenke <david.karenke@live.com>
2020-12-22 13:57:20 +0100

    cloud sql test 1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"log"
@@ -36,7 +37,7 @@ func greetingHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t, err := template.ParseFiles("templates/start.html")
-	//writeToDatabase("Buch", 2)
+	writeToDatabase("Buch", 2)
 
 	if err != nil {
 		log.Print("Error parsing template: ", err)
@@ -47,7 +48,8 @@ func greetingHandler(w http.ResponseWriter, r *http.Request) {
 func writeToDatabase(name string, anzahl int) {
 
 	log.Print("Write to DB")
-	db, err := sql.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/webapp")
+	//db, err := sql.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/webapp")
+	db, err := initSocketConnectionPool()
 	if err != nil {
 		log.Print(err.Error())
 	}
@@ -63,4 +65,31 @@ func writeToDatabase(name string, anzahl int) {
 
 	defer db.Close()
 
+}
+
+func initSocketConnectionPool() (*sql.DB, error) {
+	// [START cloud_sql_mysql_databasesql_create_socket]
+	var (
+		dbUser                 = os.Getenv("DB_USER")                  // e.g. 'my-db-user'
+		dbPwd                  = os.Getenv("DB_PASS")                  // e.g. 'my-db-password'
+		instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		dbName                 = os.Getenv("DB_NAME")                  // e.g. 'my-database'
+	)
+
+	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	if !isSet {
+		socketDir = "/cloudsql"
+	}
+
+	var dbURI string
+	dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
+
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("mysql", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+
+	return dbPool, nil
+	// [END cloud_sql_mysql_databasesql_create_socket]
 }
