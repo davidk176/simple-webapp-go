9ec5f756ee18b211722e423b5ec7739f87e6d40f
David Karenke <david.karenke@live.com>
2021-01-02 15:14:01 +0100

    add  shop v3

---
+++ controller.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type PageVar struct {
+	Title    string
+	Response string
+}
+
+type Artikel struct {
+	Name string
+	Anz  int64
+}
+
+func shoppingHandler(w http.ResponseWriter, _ *http.Request) {
+	log.Print("Start shoppingHandler")
+
+	Var := PageVar{
+		Title: "MyShop",
+	}
+	t, err := template.ParseFiles("templates/shop1.html")
+
+	if err != nil {
+		log.Print("Error parsing template: ", err)
+	}
+	err = t.Execute(w, Var)
+}
+
+func articelHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Start articelHandler")
+	_ = r.ParseForm()
+
+	log.Print(r)
+	name := r.Form.Get("name")
+	menge, _ := strconv.ParseInt(r.Form.Get("menge"), 10, 64)
+
+	Var := PageVar{
+		Title:    "MyShop",
+		Response: name,
+	}
+
+	t, err := template.ParseFiles("templates/shop1.html")
+
+	if err != nil {
+		log.Print("Error parsing template: ", err)
+	}
+	err = t.Execute(w, Var)
+
+	artikel := Artikel{
+		Name: name,
+		Anz:  menge,
+	}
+	log.Print(artikel)
+	writeToDatabase(artikel)
+
+}
--- main.go
+++ main.go
@@ -2,77 +2,25 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
 
-type PageVar struct {
-	Title    string
-	Response string
-}
-
-type Artikel struct {
-	Name string
-	Anz  int64
-}
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-	log.Print("Using  Port " + port)
+	port := getPort()
 
 	mux := http.NewServeMux()
-
-	//mux.HandleFunc("/", greetingHandler)
 	mux.HandleFunc("/", shoppingHandler)
 	mux.HandleFunc("/add", articelHandler)
 	_ = http.ListenAndServe(":"+port, mux)
 }
 
-func shoppingHandler(w http.ResponseWriter, _ *http.Request) {
-	log.Print("Start shoppingHandler")
-
-	Var := PageVar{
-		Title: "MyShop",
+func getPort() (port string) {
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
 	}
-	t, err := template.ParseFiles("templates/shop1.html")
-
-	if err != nil {
-		log.Print("Error parsing template: ", err)
-	}
-	err = t.Execute(w, Var)
-}
-
-func articelHandler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Start articelHandler")
-	_ = r.ParseForm()
-
-	log.Print(r)
-	name := r.Form.Get("name")
-	menge, _ := strconv.ParseInt(r.Form.Get("menge"), 10, 64)
-
-	Var := PageVar{
-		Title:    "MyShop",
-		Response: name,
-	}
-
-	t, err := template.ParseFiles("templates/shop1.html")
-
-	if err != nil {
-		log.Print("Error parsing template: ", err)
-	}
-	err = t.Execute(w, Var)
-
-	artikel := Artikel{
-		Name: name,
-		Anz:  menge,
-	}
-	log.Print(artikel)
-	writeToDatabase(artikel)
-
+	log.Print("Using  Port " + port)
+	return port
 }
