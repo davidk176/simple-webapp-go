ecb2c13483ed90194ca24bba9d967711046fad78
David Karenke <david.karenke@live.com>
2021-01-03 11:13:26 +0100

    secrets v1

--- dbutils.go
+++ dbutils.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"context"
 	"database/sql"
 	"fmt"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 	"log"
 	"os"
 )
@@ -51,7 +54,7 @@ func initSocketConnectionPool() (*sql.DB, error) {
 	// [START cloud_sql_mysql_databasesql_create_socket]
 	var (
 		dbUser                 = os.Getenv("DB_USER")
-		dbPwd                  = os.Getenv("DB_PASS")
+		dbPwd, err             = accessSecretVersion("pw-sqldb")
 		instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME")
 		dbName                 = os.Getenv("DB_NAME")
 	)
@@ -72,4 +75,36 @@ func initSocketConnectionPool() (*sql.DB, error) {
 
 	return dbPool, nil
 	// [END cloud_sql_mysql_databasesql_create_socket]
+}
+
+// accessSecretVersion accesses the payload for the given secret version if one
+// exists. The version can be a version number as a string (e.g. "5") or an
+// alias (e.g. "latest").
+func accessSecretVersion(name string) (*string, error) {
+	// name := "projects/my-project/secrets/my-secret/versions/5"
+	// name := "projects/my-project/secrets/my-secret/versions/latest"
+	log.Print("access Secret " + name)
+	var secret string
+
+	// Create the client.
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create secretmanager client: %v", err)
+	}
+
+	// Build the request.
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	}
+
+	// Call the API.
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to access secret version: %v", err)
+	}
+	secret = string(result.Payload.Data)
+
+	log.Print(secret)
+	return &secret, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/davidk176/simple-webapp-go
 
 go 1.15
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	cloud.google.com/go v0.74.0
+	github.com/go-sql-driver/mysql v1.5.0
+	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d
+)
