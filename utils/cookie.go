a54d84dfe90f30dc49f49f8911ea914386d5d093
David Karenke <david.karenke@live.com>
2021-01-10 15:08:52 +0100

    seperate cookie logic

---
+++ utils/cookie.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"encoding/base64"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+/*
+Value aus Cookie
+*/
+func GetInfoFromCookie(c *http.Cookie) string {
+	if c != nil {
+		log.Print("Token from Cookie: " + c.Value)
+		return c.Value
+	}
+	log.Print("Cookie is null")
+	return ""
+}
+
+/*
+schreibt Token in Cookie
+*/
+func GenerateTokenCookie(w http.ResponseWriter, n string, value string, e time.Time) {
+	cookie := http.Cookie{Name: n, Value: value, Expires: e, HttpOnly: true}
+	http.SetCookie(w, &cookie)
+	log.Print("set new cookie " + n)
+}
+
+/*
+generiert Cookie mit random state
+*/
+func GenerateStateCookie(w http.ResponseWriter) string {
+	var exp = time.Now().Add(365 * 24 * time.Hour)
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: exp, HttpOnly: true}
+	http.SetCookie(w, &cookie)
+	return state
+}
