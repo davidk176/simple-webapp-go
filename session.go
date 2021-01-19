6fbc7fd4fd0cee8410367c01f3519a2b582723e2
David Karenke <david.karenke@live.com>
2021-01-19 09:54:23 +0100

    appengine context

--- session.go
+++ session.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"google.golang.org/appengine"
 	"google.golang.org/appengine/memcache"
 	"log"
 	"net/http"
 )
 
 func writeDataInCache(res http.ResponseWriter, req *http.Request) {
-	ctx := appengine.NewContext(req)
+	ctx := req.Context()
 	m := map[string]string{
 		"email": "test@example.com",
 	}
