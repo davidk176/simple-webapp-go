c1cc7bc8494213605c049bef2902fc39512b3859
davidk176 <41790853+davidk176@users.noreply.github.com>
2021-02-02 19:39:32 +0100

    Update controller.go

--- controller.go
+++ controller.go
@@ -49,7 +49,7 @@ func shoppingHandler(w http.ResponseWriter, r *http.Request) {
 		Picture:  session.Values["picture"].(string),
 		Username: session.Values["username"].(string),
 	}
-	pv.Artikel = getArtikelFromDatabase()
+	//pv.Artikel = getArtikelFromDatabase()
 	t, err := template.ParseFiles("templates/shop1.html")
 
 	if err != nil {
